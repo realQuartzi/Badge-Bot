664a43fad707ade4151d47f5c9636011828dbaf7
realQuartzi <nightmaresdev@gmail.com>
2021-01-15 15:24:51 +0100

    Show Badges Earned & Created
    2 New Commands added:
    !s-badges (Show Badges)
    !l-badges (List Badges)

--- src/bot.js
+++ src/bot.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const fs = require('fs');
 
-const { Client } = require('discord.js');
+const { Client, MessageEmbed, DiscordAPIError } = require('discord.js');
 const client = new Client();
 
 const PREFIX = "!";
@@ -41,6 +41,21 @@ client.on('message', (message) =>
             {
                 GiveBadge(args[0], args[1], message.channel.guild.id, message.channel);
             }
+        }
+        else if(cmd == "l-badges")
+        {
+            if(!isNaN(args[0]))
+            {
+                message.channel.send(ReturnBadgeListEmbed(message.channel.guild.id, args[0]));
+            }
+            else
+            {
+                message.channel.send("Please state a valid page you would like to view");
+            }
+        }
+        else if(cmd == "s-badges")
+        {
+            message.channel.send(ReturnUserBadgesEmbed(message.author.id, message.channel.guild.id));
         }
     }
 });
@@ -107,10 +122,8 @@ function GetBadgeName(badgeID, guildID)
 {
     badgeDir = dataDir + guildID + "/Badges/" + badgeID + ".json";
 
-    console.log(badgeDir);
     if(fs.existsSync(badgeDir))
     {
-        console.log("Yes");
         var data = JSON.parse(fs.readFileSync(badgeDir));
 
         var name = data.badgeName;
@@ -166,6 +179,56 @@ function CreateUserFile(userID, guildID)
         let data = JSON.stringify(userData, null , 2);
         fs.writeFileSync(dataDir + guildID + "/Users/" + userID + ".json", data);
     }
+}
+
+function ReturnBadgeListEmbed(guildID, value)
+{
+    const embed = new MessageEmbed()
+    .setColor('#fccfff')
+    .setTitle('Available Badges')
+    .setDescription('Badges that are available in this Server');
+
+    var start = 1;
+    if(value > 1)
+    {
+        start = (value * 25) + 1;
+    }
+
+    for(i = start; i < start + 25; i++)
+    {
+        if(GetBadgeName(i, guildID) != false)
+        {
+            console.log(`Adding Badge ${GetBadgeName(i, guildID)} to list`);
+            embed.addField(`ID ${i}`, GetBadgeName(i, guildID), true);
+            
+        }
+
+    }
+
+    return embed;
+
+}
+
+function ReturnUserBadgesEmbed(userID, guildID)
+{
+    userDir = dataDir + guildID + "/Users/" + userID + ".json";
+
+    const embed = new MessageEmbed()
+    .setColor('#fccfff')
+    .setTitle('Your Badges')
+    .setDescription('All badges that where awarded to you');
+
+    var data = JSON.parse(fs.readFileSync(userDir));
+
+    for(i = 1; i < data.badgeID.length + 1; i++)
+    {
+        if(GetBadgeName(i, guildID) != false)
+        {
+            embed.addField(GetBadgeName(i, guildID), data.badgeCount[i -1], true);
+        }
+    }
+
+    return embed;
 }
 
 const badgeData = 
